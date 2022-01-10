bbe3e0415b271fcef9d264a4abf9a0b65ab7532c
superco-art <daniela.trifu@gmail.com>
2022-01-10 01:25:26 +0100

    react frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React, {useState, useEffect } from 'react';
+import { getImages, searchImages } from './api';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [imageList, setImageList] = useState([]);
+  const [nextCursor, setNextCursor] = useState(null);
+  const [searchValue, setSearchValue] = useState('');
+
+  useEffect(() => {
+    const fetchData = async() => {
+      const responseJson = await getImages();
+      setImageList(responseJson.resources);
+      setNextCursor(responseJson.next_cursor);
+    };
+
+    fetchData();
+
+  }, []);
+
+  const handleLoadMoreButtonClick = async () => {
+    const responseJson = await getImages(nextCursor);
+    setImageList((currentImageList) => [
+      ...currentImageList,
+      ...responseJson.resources,
+    ]);
+    setNextCursor(responseJson.next_cursor);
+  };
+
+  const handleFormSubmit = async (event) => {
+      event.preventDefault();
+  
+    const responseJson = await searchImages(searchValue, nextCursor);
+  setImageList(responseJson.resources);
+  setNextCursor(responseJson.next_cursor);
+};
+
+const resetForm = async () => {
+  const responseJson = await getImages();
+  setImageList(responseJson.resources);
+  setNextCursor(responseJson.next_cursor);
+
+  setSearchValue('');
+
+};
+
+  
+   return (
+     <>
+     <form onSubmit={handleFormSubmit}>
+       <input value={searchValue} 
+              onChange={(event)=> setSearchValue(event.target.value)}
+              required='required'
+              placeholder='Enter here your search description'></input>
+       <button type='submit'>Search</button>
+        <button type='button' onClick={resetForm}>Clear</button>
+     </form>
+
+    <div className="image-grid"> 
+    {imageList.map((image)=> (
+    <img src={image.url} alt={image.public_id}></img>
+    ))}
     </div>
+    <div className="footer">
+      {nextCursor && (
+      <button onClick={handleLoadMoreButtonClick}>Load more</button>
+      )}
+    </div>
+    </>
   );
-}
+};
 
 export default App;
